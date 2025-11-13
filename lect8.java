1dd88123c529dca0ad1033d7d1d005a74c5f53a8
Ayush Gupta <ag6320226@gmail.com>
2025-11-13 20:28:56 +0530

    class

---
+++ lect8.java
@@ -0,0 +1,100 @@
+
+//     class Hello{
+//         public Hello(){
+//             this(5);//two variabl ko different karta hai
+//             System.out.println("hiii");
+//         }
+    
+//     public Hello(int a){
+//         this(5,6);
+//         System.out.println(a);
+//     }
+//     public Hello(int a,int b){
+//         System.out.println(a);
+//     }
+//    public class lect8{
+//     public static void main(String[] args){
+//         Hello obj=new Hello();
+             
+       
+//     }
+    
+// }
+//     }
+// class Animal{
+//     public void eat(){
+//         System.out.println("pizza");
+//     }
+// }
+// class Dog extends Animal{
+//     void bark(){
+//         System.out.println("woof");
+
+//     }
+// }
+// class puppy extends Dog{
+//     void food(){
+//         System.out.println("milk");
+
+//     }
+// }
+// class lect8{
+//     public static void main(String[] args) {
+//         Dog d=new Dog();
+//         puppy p=new puppy();
+//         p.food();
+//         d.bark();
+//         d.eat();
+       
+//     }
+// }
+
+// class employee has method work()
+//class Managerinherits employee and has method attendingmeeting()
+//create an object of manager and call both method
+// class Employee{
+//     void work(){
+//         System.out.println("working");
+//     }
+// }
+// class Manager{
+//     void attendingmeeting(){
+//         System.out.println("attendingmeeting");
+
+//     }
+// }
+// class lect8{
+//     public static void main(String[] args) {
+//         Manager m=new Manager();
+//         m.attendingmeeting();
+//         Employee e=new Employee();
+//         e.work();
+
+
+
+
+//     }
+// }
+    // class Person{
+    //     void displayName(){
+    //         System.out.println("Name:Aryan");
+    //     }
+    // }
+    // class Student extends Person{
+    //     void displayClass(){
+    //         System.out.println("Class:12th");
+    //     }
+    // }
+    // class Moniter extends Student{
+    //     void checkDiscipline(){
+    //         System.out.println("checking discipline");
+    //     }
+    // }
+    // public class lect8{
+    //     public static void main(String[] args) {
+    //         Moniter mon=new Moniter();
+    //         mon.displayName();
+    //         mon.displayClass();
+    //         mon.checkDiscipline();
+    //     }
+    // }
