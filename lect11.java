0a756595c3de7b2202cd060503de3efc5de12b06
Ayush Gupta <ag6320226@gmail.com>
2025-11-20 14:29:56 +0530

    interfce

---
+++ lect11.java
@@ -0,0 +1,90 @@
+// abstract class Shape{
+//    abstract void area();
+  
+// }
+// class Circle extends Shape{
+//   int r;
+//   public Circle(int r){
+//    this.r=r;
+//   }
+//   void area(){
+//    double result=3.14*r*r;
+//    System.out.println(result);
+
+//   }
+
+
+//    }
+//    class Rectangle extends Shape{
+//       int length;
+//       int b;
+//       public Rectangle(int length,int b){
+//          this.length=length;
+//          this.b=b;
+         
+//       }
+//       void area(){
+//          double result=length*b;
+//          System.out.println(result);
+//       }
+//    }
+//     public class lect11{
+//       public static void main(String[] args) {
+//          Circle c1=new Circle(6);
+//          c1.area();
+//          Rectangle d1=new Rectangle(3, 6);
+//          d1.area();
+//       }
+//     }
+//interface
+// interface A{
+//     abstract void login();
+// }
+// class B implements A{
+//     public void login(){
+//         System.out.println("hii");
+
+//     }
+// }
+// public class lect11{
+//     public static void main(String[] args) {
+//         A obj=new B();
+//         obj.login();
+       
+     
+//     }
+// }
+//multiple inheritance using interface
+// ?
+// interface A{
+//    void login();
+// //    default void data(){
+//     // System.out.println("hhi");
+//    }
+
+
+// interface B{
+//    void signup();  
+
+// }
+    
+// class C implements A{
+//     public void login(){
+//         System.out.println("hii");
+//     }
+// }
+// public void signup(){
+//     System.out.println("hello");
+// }
+
+// public class lect11{
+
+//     public static void main(String[] args) {
+//         C obj=new C();
+//         obj.login();
+        // obj.signup(
+//     }
+// }
+// interface A{
+
+// });
