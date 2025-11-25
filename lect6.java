07419b28e35a8de7fa5675b82b8f62eae35f9e59
Ayush Gupta <ag6320226@gmail.com>
2025-11-25 23:20:36 +0530

    data

---
+++ lect6.java
@@ -0,0 +1,99 @@
+
+//  class lect {
+//     static  int a=5;
+
+//     // public  void sum(){
+//     //     System.out.println("hello");
+//     // }
+//     public static void main(String[] args) {
+      
+//         // sum();
+//         hello obj =new hello();
+//         System.out.println(obj.a);
+//         // obj.sum();
+        
+//     }
+    
+// }
+
+
+// class Main{
+// //    static  int a=5; 
+//  void  Sum(int a){
+//  // "aabbcd"
+//     System.out.println("hello");
+// }
+// static  int val(){
+//     return  0;
+
+// };
+//     public static void main(String[] args) {
+//         // Sum();
+//         Main obj=new Main();
+//         int data=9;
+//         obj.Sum(data);
+//         val();
+
+//         // System.out.println(a);
+//         // int a=5;
+//         // System.out.println(a);
+//         // System.out.println(a);
+//     }
+// }
+
+
+// public class hello{
+//     public static void main(String[] args) {
+//         // Main obj=new Main();
+//         // System.out.println(obj);
+//         Car obj=new Car();
+//         obj.display();  
+//     }
+// }          
+//    class Car{
+//        String name="hiii";
+//      void display(){
+//         System.out.println(name);
+        
+//     }
+// }
+
+// class hello{
+// //   static  int a=5;
+// //    static  int data=20;
+//    static  int  Sum(int a){
+//         System.out.println("hello");
+//         return 0;
+//     }
+
+//     public static void main(String[] args) {
+//         // Sum();
+//         // int a=5;
+//         Sum(5);
+//         // hello obj=new hello();
+//         // int data=5;
+//         // obj.Sum(data);
+//         // // System.out.println(a);
+//         // System.out.println(obj.data);
+
+//     }
+
+
+// }
+class Car{
+     String name="hello";
+    static  void Sum(){
+        Car a=new Car();
+        System.out.println(a.name);
+    }
+}
+
+public class lect6{
+    public static void main(String[] args) {
+        Car obj =new Car();
+        obj.Sum();
+        
+    }
+   
+}
+  
