cc7a4475b97739afde78113fa74487991fd85244
Ayush Gupta <ag6320226@gmail.com>
2025-11-25 23:19:33 +0530

    basic constructor

---
+++ lect4.java
@@ -0,0 +1,169 @@
+// public class lect4 {
+//     public static void main(String[] args){
+//         Student s1=new Student();
+//         s1.name="ram";
+//         s1.roll=1;
+//         s1.marks=90;
+//         s1.display();
+//         Student s2=new Student();
+//         s2.name="ashu";
+//         s2.roll=2;
+//         s2.marks=80;
+//         s2.display();
+
+//         Student s3=new Student();
+//         s3.name="syam";
+//         s3.roll=3;
+//         s3.marks=70;
+//         s3.display();
+//     }
+// class Student{
+//     String name;
+//     int roll;
+//     int marks;
+//     void display(){
+//         System.out.println("name+" "+roll+" "+marks");
+
+//     }
+    
+// }
+// }
+
+
+
+
+        
+//         //         Car obj=new Car();
+// //         obj.display();
+// //     }
+// // //     static int a=6;
+// // void sum(int a){
+// //     System.out.println("hello");
+// // }
+
+// //     public static void main(String[] args){
+// //         Main obj=new Main();
+// //     //    obj.sum(5);
+// // //         Main obj=new Main();
+// // //         int b=9;
+// //         System.out.println(a);
+// // //         System.out.println(b);
+// //     }
+     
+    
+// // }
+// //aabbcd  non repeating chlect4/h
+// // import java.util.Arrays;
+// // public class hello {
+// //     public static void main(String[] args) {
+// //         String str="hello";
+// //         String newStr="";
+// //         for(char ch : str.toCharArray()){
+// //             if(newStr.indexOf(ch)==-1){
+// //                 newStr+=ch;
+// //             }
+// //         }
+// //         System.out.println(newStr);
+        
+// //     }
+    
+// // }
+
+
+// //freq check of each char
+
+// // class Main{
+// //     public static void main(String[] args) {
+
+// //     //     int freq[]=new int[256];
+// //     //   // java is programing lang
+// //     //      String str="banana";
+// //     //      for(char ch :str.toCharArray()){
+// //     //         freq[ch]++;
+// //     //      }
+// //         //  for(int i=0;i<256;i++){
+// //         //     if(freq[i]>0){
+// //         //         System.out.println((char)i +"-> " + freq[i]);
+// //         //     }
+// //         //  }
+// //         // String str="hello";
+// //         // String newStr="";  
+// //         // for(char ch :str.toCharArray()){
+// //         //     if(newStr.indexOf(ch)==-1){
+// //         //         newStr+=ch;
+
+// //         //     }
+// //         // }
+// //         // System.out.println(newStr);
+
+        
+// //     }
+// // }
+
+    
+
+
+// //Anagram check
+
+// // class Hello{
+// //     // public static void main(String[] args) {
+// //         String  a="hello";
+// //         String  b="lloeh";
+// //         char ch1[]=a.toCharArray();
+// //         char ch2[]=b.toCharArray();
+// //         Arrays.sort(ch1);
+// //         Arrays.sort(ch2);
+// //             String str1=new String(ch1);
+// //             String str2=new String(ch2);
+// //         if(str1.equals(str2)){
+// //             System.out.println(true);
+// //         }else{
+// //             System.out.println(false);
+// //         }
+
+
+
+
+
+
+
+// //         // String str="java is powerful";
+// //         // String a[]=str.split(" ");
+// //         // String lagest="";
+// //         // for(String val : a){
+// //         //     if(val.length()>lagest.length()){
+// //         //         lagest=val;
+// //         //     }
+// //         // }
+// //         // System.out.println(lagest);
+
+
+
+        
+// //     }
+// // }
+// // classese multiple kaise banti hai'
+// // public class Car{
+// //     String name="hii";
+// //     void display(){
+// //         System.out.println("hello");
+// //     }
+
+// // }
+// // }
+// //create class stu. with fields nome.roll,marks 3 student
+
+//create a clsas rectangle with length and bteadth
+class Rectangle{
+    int length;
+    int breadth;
+    Rectangle(int l,int b){
+        length=1;
+        breadth=b;
+     
+
+        
+
+    }
+
+}
