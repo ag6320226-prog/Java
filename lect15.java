00f437b64c7346713db5865d93fe5d78fbc6b961
Ayush Gupta <ag6320226@gmail.com>
2025-11-26 22:45:04 +0530

    hashmap

---
+++ lect15.java
@@ -0,0 +1,60 @@
+// import java.util.HashMap;
+
+// public class lect15 {
+//    public static void main(String[] args) {
+//     //  HashMap<Integer,Integer> map=new HashMap<>();
+
+
+//     int arr[]={2,3,6,7,3};
+//        HashMap<Integer,Integer> map=new HashMap<>();
+//        int target=9;
+//        for(int i=0;i<arr.length;i++){
+//         int diff=target-arr[i];
+//         if(map.containsKey(diff)){
+//             System.out.println(map.get(diff)+ " "+i);
+//             break;
+//         }
+//         map.put(arr[i],i);
+//        }
+//     }
+// }
+
+    // first non repeating char
+//     String str="aabcaddbgavgf";
+//     HashMap<Character,Integer> map=new HashMap<>();
+//     for(char ch:str .toCharArray()){
+//    map.put(ch,map.getOrDefault(ch,0)+1);
+//     }
+//     System.out.println(map);
+//     for(char ch:str.toCharArray()){
+//         if(map.get(ch)==1){
+//             System.out.println(ch);
+//             break;
+//         }
+//     }
+// }
+// }
+    
+
+
+//     map.put(1, 30);
+//     map.put(2, 60);
+//     map.put(45, 20);
+
+//     System.out.println(map);
+//     System.out.println(map.get(1));
+//     System.out.println(map.getOrDefault(11, 10));
+//     for(int x:map.keySet()){
+//         System.out.println(x);
+//     }
+//    }    
+// }
+// int arr[]={1,2,3,4,2,3,4,2,2,4,2,4};
+// for(int x: arr){
+//     map.put(x,map.getOrDefault(x, 0)+1);//ferq count
+// }
+// System.out.println(map);
+//    }
+// }
+
+
